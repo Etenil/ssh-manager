e2f917b2b78d9325d21b9570b7cd907173a67e77
Guillaume Pasquet <guillaume.pasquet@nccgroup.trust>
2016-10-11 14:59:24 +0100

    Updated icon to accomodate Windows.

--- sshmanager.py
+++ sshmanager.py
@@ -108,7 +108,7 @@ class SshManager:
                 print("  " * indent + b'\xf0\x9f\x93\x82'.decode('utf8') + ' ' + key)
                 self._print_hierarchy(item[key], indent + 1)
             else:
-                print("  " * indent + b'\xf0\x9f\x96\xb3'.decode('utf8') + ' ' + key)
+                print("  " * indent + b'\xf0\x9f\x92\xbb'.decode('utf8') + ' ' + key)
     
     def _print_connection(self, connection):
         for key in connection:
